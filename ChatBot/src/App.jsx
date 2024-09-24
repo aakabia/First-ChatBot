9bcff6558fe9ab8e81240979b46d238d6045bbb7
Aziz Kabia <158849915+aakabia@users.noreply.github.com>
2024-09-24 14:01:13 -0400

    made my react set up

---
+++ ChatBot/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div >Hello</div>
+      <div >Friend!</div>
+      <span class="material-symbols-outlined">search</span>
+    </>
+  );
+}
+
+export default App;
