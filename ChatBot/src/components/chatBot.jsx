de2b2aa0fa98474352e51633ff48ad74df3a1068
Aziz Kabia <158849915+aakabia@users.noreply.github.com>
2024-10-08 22:10:40 -0400

    made chatbot visibilty toggle

--- ChatBot/src/components/chatBot.jsx
+++ ChatBot/src/components/chatBot.jsx
@@ -1,6 +1,32 @@
+import React, { useEffect, useState } from 'react';
+import { io } from "socket.io-client";
+import { toggleChatBot } from "../utils/helpers";
+
+
 function ChatBot() {
+  
+  useEffect(() => {
+
+    const cleanup = toggleChatBot();
+    // Above, we call toggleChatBot and assign its return function to clean up 
+    // Even though we attach to variable, as we call the event listener is active when component mounted
+    
+    return () => {
+      if (cleanup) cleanup();
+
+    };
+    // Above, we use our cleanup fucntion to remove event listener if cleanup exists. 
+    // This happens when component  unmounts 
+
+  }, []);
+  // The purpose of useEffect is to manage side effects in functional components.
+  // The return function is optional for clean up but is good practice.
+  // The empty array is the dependency, empty array means this will run only once and that is when component first mounts.
+
+
+
   return (
-    <section className="container-fluid showChatBot ">
+    <section className="container-fluid showChatBot " id = "chatSection" >
       <div className="row ">
         <button className="chatBot-toggler">
           <span className="material-symbols-outlined">mode_comment</span>
